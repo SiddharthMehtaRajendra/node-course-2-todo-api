5899499d63ebf48a6e5034e7578a715bc376571e
Siddharth Mehta <siddharthmehta@Siddharths-MacBook-Pro.local>
2018-07-14 23:01:33 +0530

    Files

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
+require('./config/config.js');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('../model/todos.js');
-var {User} = require('../model/user.js');
+var {Todo} = require('../model/todos');
+var {User} = require('../model/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -88,7 +90,7 @@ app.patch('/todos/:id', (req, res) => {
         if(!todo){
             return res.status(400).send();
         }
-        
+
         res.send({todo});
     }).catch((e) => {
         res.status(400).send();
