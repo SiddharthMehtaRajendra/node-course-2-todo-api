7faf763563eed405860d9e886c8086821ca6a8c6
Siddharth Mehta <siddharthmehta@Siddharths-MacBook-Pro.local>
2018-07-14 20:37:51 +0530

    Mongoose Remove

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,5 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoo');
+const {Todo} = require('./../model/todos.js');
+const {User} = require('./../model/user.js');
